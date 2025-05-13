2ab3829667345bbe56456d69098addb5f40e07ce
Younous TCHAO <younoustchao@gmail.com>
2025-05-13 16:36:14 +0000

    Ajout des listener

--- src/ihm/MenuBarSample.java
+++ src/ihm/MenuBarSample.java
@@ -40,26 +40,59 @@ public class MenuBarSample extends JFrame {
         mnuOpenFile.setIcon(new ImageIcon("icons/open.png"));
         mnuOpenFile.setMnemonic(('O'));
         mnuOpenFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
+        mnuOpenFile.addActionListener(this::mnuOpenListener);
         mnuFile.add(mnuOpenFile);
+
+        mnuFile.addSeparator();
 
         JMenuItem mnuSaveFile = new JMenuItem("Save File ...");
         mnuSaveFile.setIcon(new ImageIcon("icons/save.png"));
         mnuSaveFile.setMnemonic('S');
         mnuSaveFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
+        mnuSaveFile.addActionListener(this::mnuSaveListener);
         mnuFile.add(mnuSaveFile);
 
+        mnuFile.addSeparator();
+
         JMenuItem mnuSaveFileAs = new JMenuItem("Save File As ...");
-        mnuSaveFileAs.setIcon(new ImageIcon("icons/saveas.png"));
-        mnuSaveFileAs.setMnemonic('s');
+        mnuSaveFileAs.setIcon(new ImageIcon("icons/save_as.png"));
+        mnuSaveFileAs.setMnemonic('A');
         mnuSaveFileAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_SHIFT, KeyEvent.CTRL_DOWN_MASK));
+        mnuSaveFileAs.addActionListener(this::mnuSaveAsListener);
         mnuFile.add(mnuSaveFileAs);
 
+        mnuFile.addSeparator();
 
+        JMenuItem mnuExit = new JMenuItem("Exit");
+        mnuExit.setIcon(new ImageIcon("icons/exit.png"));
+        mnuExit.setMnemonic('X');
+        mnuExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, KeyEvent.ALT_DOWN_MASK));
+        mnuExit.addActionListener(this::mnuExitListener);
+        mnuFile.add(mnuExit);
+
+        mnuFile.addSeparator();
         return menuBar;
     }
 
     private void mnuNewListener(ActionEvent event) {
         JOptionPane.showMessageDialog(this, "Button Clicked !");
     }
+
+    private void mnuOpenListener(ActionEvent event) {
+        JOptionPane.showMessageDialog(this, "Button Clicked !");
+    }
+
+    private void mnuSaveListener(ActionEvent event) {
+        JOptionPane.showMessageDialog(this, "Button Clicked !");
+    }
+
+    private void mnuSaveAsListener(ActionEvent event) {
+        JOptionPane.showMessageDialog(this, "Button Clicked !");
+    }
+
+    private void mnuExitListener(ActionEvent event) {
+        System.exit(0);
+    }
+
 
 }
